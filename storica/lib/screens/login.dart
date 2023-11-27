a2efad369425443a0fdef1313442db15caed70ec
bangjai123 <znaufalilmi@gmail.com>
2023-11-27 16:38:19 +0700

    base code

--- storica/lib/screens/login.dart
+++ storica/lib/screens/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
+import 'package:storica/screens/menu.dart';
 
 void main() {
   runApp(const LoginApp());
